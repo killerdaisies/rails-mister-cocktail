3b18bcd2c7a54213f7c7c1bcbc3a048fc099aa48
jane gong <janegongxw@gmail.com>
2018-07-27 16:28:36 +0800

    some stuff done

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,21 @@
 require 'open-uri'
 require 'json'
 
-Ingredient.delete_all
-puts "Deleted all ingredients"
+# Ingredient.delete_all
+# puts "Deleted all ingredients"
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 cocktail = open(url).read
 results = JSON.parse(cocktail)
 
-results["drinks"].each do |result|
-  Ingredient.create!(name: result["strIngredient1"]
-    # remote_photo_url: urls.sample)
+ingredients = results["drinks"].map do |result|
+  result["strIngredient1"]
+end
+
+ingredients.sort!
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient)
 end
 
 puts "created #{Ingredient.count} ingredients!"
